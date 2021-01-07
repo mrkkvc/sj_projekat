debf06e2e7f2dee7a865a10564989b24120fc698
Marko <mkovac10220rn@raf.rs>
2021-01-07 18:14:01 +0100

    Projekat set up

--- teretana/apps.py
+++ teretana/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class TeretanaConfig(AppConfig):
-    name = 'Teretana'
+    name = 'teretana'
---
+++ teretana/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Card(models.Model):
+    description = models.CharField(max_length=20)
+    price = models.IntegerField(max_length=10)
+
+    def str(self):
+        return self.description
+
+class Member(models.Model):
+    name = models.CharField(max_length=50)
+    card_type = models.ForeignKey(Card, on_delete = models.CASCADE)
+
+    def str(self):
+        return self.naziv
+
